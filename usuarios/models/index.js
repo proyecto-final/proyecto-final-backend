aba1f3d930a7eebdc46e5ba87564c14ea42facb6
jjf-covello <105680112+bugmaker13@users.noreply.github.com>
2022-06-02 23:35:18 -0300

    fix dockerfiles

--- usuarios/models/index.js
+++ usuarios/models/index.js
@@ -1,23 +1,9 @@
-// Option 1: Passing a connection URI
-//for tests with mock db
-//prod
-//const sequelize = new Sequelize('mysql://SherlockRoot:SH3rl0ckR00t@database-testing.csd1nltwgzc6.us-east-1.rds.amazonaws.com:3306/test')
-//testing
-
-/* segun la docu
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect:  one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' 
-})
-*/ 
-
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 require('dotenv').config()
 const basename = path.basename(__filename)
 const db = {}
-// TODO: change this to a config file
 const sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING)
 fs
   .readdirSync(__dirname)
