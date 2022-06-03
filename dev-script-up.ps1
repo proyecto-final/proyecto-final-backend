9ae23d7dad1f732d3651414d438d166761500d4d
jjf-covello <105680112+bugmaker13@users.noreply.github.com>
2022-06-03 01:21:40 -0300

    add powershell script for dev setup
    Also now it opens a window with nodemon execution :star:

--- dev-script-up.ps1
+++ dev-script-up.ps1
@@ -4,30 +4,13 @@ docker build -t mysql:dev mysql
 docker run --name mongo-dev-container -d -p 27014:27014 mongo:dev 
 docker run --name mysql-dev-container -d -p 3306:3306 mysql:dev 
 
-cd busqueda
-npm i &
-npm run dev &
-cd ..
 
-cd ips
-npm i &
-npm run dev &
-cd ..
+@("busqueda","ips","correlacion","timeline", "usuarios")  | foreach {
+     $command =  "/c run-module.bat $_"
+     Start-Process 'cmd' -ArgumentList $command
+    }
 
-cd correlacion
-npm i &
-npm run dev &
-cd ..
 
-cd timeline
-npm i &
-npm run dev &
-cd ..
-
-cd usuarios
-npm i &
-npm run dev &
-cd ..
 
 echo "Corrio todo! Proba los siguientes modulos: "
 echo "usuarios -> 3030"
