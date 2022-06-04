be1a0a57963ccc6ef9fe0e9567bc734312be4276
bugmaker13 <jcovello@frba.utn.edu.ar>
2022-06-04 19:32:49 -0300

    add fixes to connect in relational database

--- usuarios/index.js
+++ usuarios/index.js
@@ -10,11 +10,22 @@ app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}`)
 })
 
-// TODO: make this depend on env variable
+const connectToDatabase = async() => {
+  db.sequelize.sync({ force: true }).then(() => {
+    console.log('SUCESSFULLY CONNECTED!')
+    console.log('-----------------------Database sync finish! -----------------------')
+  }).catch((err) => {
+     if(err.original.code == 'PROTOCOL_CONNECTION_LOST'){
+      setTimeout(async() => {
+        console.log('Database not ready yet. Trying to re-connect')
+        await connectToDatabase()
+      }, 3000) //wait 3 seconds before reconnect
+    } else {
+      console.log('ERROR ON DATABASE SYNC')
+      console.log(err)
+     }
+  })
+}
+
 console.log('-----------------------Init database sync!-----------------------')
-db.sequelize.sync({ force: true }).then(() => {
-  console.log('-----------------------Database synced-----------------------')
-}).catch((err) => {
-  console.log('ERROR ON DATABASE SYNC')
-  console.log(err)
-})
+connectToDatabase()
