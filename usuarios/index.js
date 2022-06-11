3e74460cf07d2b80e2f332e29f97aa4c7e4085c7
bugmaker13 <jcovello@frba.utn.edu.ar>
2022-06-10 22:48:04 -0300

    add swagger screen /api-docs

--- usuarios/index.js
+++ usuarios/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const db = require('./models/index')
 require('dotenv').config()
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 const app = express()
+
+const db = require('./models/index')
 app.use(express.json())
 app.use('/api', require('./routes'))
 
@@ -10,22 +12,44 @@ app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}`)
 })
 
-const connectToDatabase = async() => {
+
+const setSwagger = () => {
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Sherlock User Module API",
+        version: "1.0.0",
+        description: "Sherlock security user module interface",
+      },
+      servers: [
+        {
+          url: `http://localhost:${process.env.PORT}`,
+        }
+      ],
+    },
+    apis: ["./routes/*.js"]
+  }
+  const specs = swaggerJSDoc(options)
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+}
+const connectToDatabase = async () => {
   db.sequelize.sync({ alter: true }).then(() => {
     console.log('SUCESSFULLY CONNECTED!')
     console.log('-----------------------Database sync finish! -----------------------')
   }).catch((err) => {
-     if(err.original.code == 'PROTOCOL_CONNECTION_LOST'){
-      setTimeout(async() => {
+    if (err.original.code == 'PROTOCOL_CONNECTION_LOST') {
+      setTimeout(async () => {
         console.log('Database not ready yet. Trying to re-connect')
         await connectToDatabase()
       }, 3000) //wait 3 seconds before reconnect
     } else {
       console.log('ERROR ON DATABASE SYNC')
       console.log(err)
-     }
+    }
   })
 }
 
 console.log('-----------------------Init database sync!-----------------------')
 connectToDatabase()
+setSwagger()
