bcb51a98bac896c8eed59e55d2a0e0666669d4b9
rdiazutn <rodrigodiaz@frba.utn.edu.ar>
2022-07-03 20:30:06 -0300

    Added organization and user query

--- usuarios/seeders/20220629195716-default-organizations-and-users.js
+++ usuarios/seeders/20220629195716-default-organizations-and-users.js
@@ -47,6 +47,7 @@ module.exports = {
         password: '7c9e7c1494b2684ab7c19d6aff737e460fa9e98d5a234da1310c97ddf5691834',
         email: 'email1@gmail.com',
         name: 'pepe',
+        enabled: true,
         createdAt: new Date(),
         updatedAt: new Date(),
         organizationId: organizationIds[0]
@@ -56,6 +57,7 @@ module.exports = {
         password: '321cb76b7d6e43ef6917ee54fd8fa0e7fa99ba97c7e0b6fc7866139e7cb8c9c3',
         email: 'email2@gmail.com',
         name: 'pepa',
+        enabled: true,
         createdAt: new Date(),
         updatedAt: new Date(),
         organizationId: organizationIds[0]
@@ -65,6 +67,7 @@ module.exports = {
         password: 'd50d3319bccca99d3093b689745b168cc79ecfd0e18e3e80be6d8c6ad1061407',
         email: 'email3@gmail.com',
         name: 'pepo',
+        enabled: true,
         createdAt: new Date(),
         updatedAt: new Date(),
         organizationId: organizationIds[1]
