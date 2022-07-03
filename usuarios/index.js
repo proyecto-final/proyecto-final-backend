0a38d91b08e527fd1f2aa1f57b743d954ff868af
bugmaker13 <jcovello@frba.utn.edu.ar>
2022-07-03 15:45:51 -0300

    add new script for docker and fix the race condition on the seeder commands

--- usuarios/index.js
+++ usuarios/index.js
@@ -36,16 +36,21 @@ const setSwagger = () => {
   const specs = swaggerJSDoc(options)
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 }
-const executeMigrations = async () => {
-  return await new Promise((resolve, reject) => {
+
+const runCommand = (stringCommand) => {
+  return new Promise((resolve, reject) => {
     const migrate = exec(
-      'npx sequelize-cli db:seed:undo:all & npx sequelize-cli db:seed:all',
+      stringCommand,
       { env: process.env },
       err => (err ? reject(err) : resolve())
     )
     migrate.stdout.pipe(process.stdout)
     migrate.stderr.pipe(process.stderr)
   })
+}
+const executeMigrations = async () => {
+  await runCommand('npx sequelize-cli db:seed:undo:all')
+  await runCommand('npx sequelize-cli db:seed:all')
 }
 const connectToDatabase = async () => {
   db.sequelize.sync({ alter: true, force: true }).then(async () => {
@@ -58,12 +63,12 @@ const connectToDatabase = async () => {
     console.log('SUCESSFULLY MIGRATED!')
     console.log('-----------------------Database sync finish! -----------------------')
   }).catch((err) => {
-      setTimeout(async () => {
-        console.log('Database not ready yet. Trying to re-connect')
-        await connectToDatabase()
-      }, 3000) //wait 3 seconds before reconnect
-      console.log('ERROR ON DATABASE SYNC')
-      console.log(err)
+    setTimeout(async () => {
+      console.log('Database not ready yet. Trying to re-connect')
+      await connectToDatabase()
+    }, 3000) //wait 3 seconds before reconnect
+    console.log('ERROR ON DATABASE SYNC')
+    console.log(err)
   })
 }
 if(process.env.ENVIRONMENT === 'DEV') {
