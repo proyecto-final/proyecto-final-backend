534eee9f8f7ee6c9283d42cf270880b91e482428
rdiazutn <rodrigo.diaz@frba.utn.edu.ar>
2022-07-11 19:40:04 -0300

    Fixed seeders

---
+++ usuarios/models/project.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  const Project = sequelize.define('project', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [2,32] },
+      unique: true
+    },
+    prefix: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [2,16] },
+      unique: true
+    },
+    color:  {
+      type: DataTypes.STRING,
+      defaultValue: '#6492E6',
+      allowNull: false,
+    }
+  })
+  Project.associate = (models) => {
+    Project.belongsTo(models.organization,
+      {foreignKey: {name: 'organizationId'}}
+    )
+  }
+  return Project
+}
