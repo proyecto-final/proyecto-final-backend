4229ba95b695f789ea309df1a38da38bcfe0e0f6
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-07-12 00:44:22 -0300

    Update rules.js

--- usuarios/controllers/utils/rules.js
+++ usuarios/controllers/utils/rules.js
@@ -2,7 +2,7 @@
 const checkColor = (color) => {
   const colorRegex = /^#([A-Fa-f0-9]{6})$/g
   if (color && !colorRegex.test(color)){
-    throw {code: 400, msg: 'Invalid color'}
+    throw {code: 400, msg: 'Color invalido'}
   }
 }
 
