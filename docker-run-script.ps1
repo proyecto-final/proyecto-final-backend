719d38569d94262807c68c521a64af8dbf03f274
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-07-20 02:19:35 -0300

    add docker magic

--- docker-run-script.ps1
+++ docker-run-script.ps1
@@ -1,5 +1,21 @@
 param([String[]] $modules = "all")
 docker-compose stop
 docker-compose rm -f
-Write-Host $modules
+rm -r ./usuarios/temp
+rm -r ./timeline/temp
+rm -r ./correlacion/temp
+rm -r ./busqueda/temp
+rm -r ./ips/temp
+
+mkdir ./usuarios/temp
+mkdir ./timeline/temp
+mkdir ./correlacion/temp
+mkdir ./busqueda/temp
+mkdir ./ips/temp
+
+cp -r ./shared ./usuarios/temp/shared
+cp -r ./shared ./timeline/temp/shared
+cp -r ./shared ./correlacion/temp/shared
+cp -r ./shared ./busqueda/temp/shared
+cp -r ./shared ./ips/temp/shared
 docker-compose --profile $modules up --build -d
