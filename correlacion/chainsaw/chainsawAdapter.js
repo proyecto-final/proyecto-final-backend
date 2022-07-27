cba74f31b0ba9c2da5f3131a1e807c8d7da5cbac
rdiazutn <rodrigo.diaz@frba.utn.edu.ar>
2022-07-27 20:41:19 -0300

    wrote files

--- correlacion/chainsaw/chainsawAdapter.js
+++ correlacion/chainsaw/chainsawAdapter.js
@@ -1,6 +1,10 @@
 async function processLog (log) {
   // 1 Write
   console.log(log)
+  const temporaryName = `${log.log._id.toString()}-${log.file.name}`
+  await log.file.mv(`${__dirname}/input/${temporaryName}`, function(err) {
+    throw { code: 500, msg: 'Error moving file'+err }
+  })
 // 2 Process
 // 3 Read output
 }
