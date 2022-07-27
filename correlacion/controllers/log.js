75e002d270cacc1bb5c72320f206006f369c5581
rdiazutn <rodrigo.diaz@frba.utn.edu.ar>
2022-07-27 20:34:07 -0300

    commit parcial

--- correlacion/controllers/log.js
+++ correlacion/controllers/log.js
@@ -3,31 +3,51 @@ const { getIntValue } = require('./../../shared/utils/dataHelpers')
 const mongoose = require('mongoose')
 const Log = require('./../../shared/models/log')(mongoose)
 const {adaptMongoosePage} = require('./../../shared/utils/pagination')
+const  {processFiles}= require('../chainsaw/chainsawAdapter.js')
 
+const checkLogs = (fileOrFiles, metadata) => {
+  if(fileOrFiles?.length === 0 || !fileOrFiles) {
+    throw { code: 400, msg: 'No files were uploaded.' }
+  }
+  const files = Array.isArray(fileOrFiles) ? fileOrFiles : [fileOrFiles]
+  if (files.length > 5) {
+    throw { code: 400, msg: 'Max 5 files are allowed.' }
+  }
+  let metadatas
+  try {
+    metadatas = JSON.parse(metadata)
+  } catch (err) {
+    throw { code: 400, msg: 'Invalid metadata, must be a valid JSON' }
+  }
+  if (files.length !== metadatas.length) {
+    throw { code: 400, msg: 'Number of files and metadata must be the same' }
+  }
+  return { files, metadatas }
+}
 const create = new RequestWrapper()
   .hasId('projectId')
   .setHandler(async (req, resp) => {
     const fileOrFiles = req.files?.files
-    if(fileOrFiles?.length === 0 || !fileOrFiles) {
-      throw { code: 400, msg: 'No files were uploaded.' }
-    }
-    const files = Array.isArray(fileOrFiles) ? fileOrFiles : [fileOrFiles]
-    if (files.length > 5) {
-      throw { code: 400, msg: 'Max 5 files are allowed.' }
-    }
-    let metadatas
-    try {
-      metadatas = JSON.parse(req.body.metadata)
-    } catch (err) {
-      throw { code: 400, msg: 'Invalid metadata, must be a valid JSON' }
-    }
-    if (files.length !== metadatas.length) {
-      throw { code: 400, msg: 'Number of files and metadata must be the same' }
-    }
-    const logsWithMetadata = files.map((file, index) => ({extension :file.name.split('.').pop(), ...metadatas[index], projectId: getIntValue(req.params.projectId)}))
+    const { files, metadatas } = checkLogs(fileOrFiles, req.body.metadata)
+    const logsWithMetadata = files.map((file, index) => 
+      ({
+        ...metadatas[index],
+        projectId: getIntValue(req.params.projectId),
+        extension: file.name.split('.').pop(),
+        file,
+      }))
+    // HEADER
     const logs = logsWithMetadata.map(logMetadata => new Log({...logMetadata, projectId: getIntValue(req.params.projectId)}))
     await Promise.all(logs.map(async log => await log.validate()))
     await Promise.all(logs.map(async log => await log.save()))
+    // BODY
+    const logsWithFiles = logs.map((log,index) => ({
+      log,
+      file: files[index]
+    }))
+    const processedLogs = await processFiles(logsWithFiles)
+    console.log(processedLogs)
+
     resp.status(200).json(logs)
   }).wrap()
 
