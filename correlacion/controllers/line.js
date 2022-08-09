e0d08b89ce0129f4809214307eed062a22bd633f
rdiazutn <rodrigodiaz@frba.utn.edu.ar>
2022-08-08 21:29:15 -0300

    fix

--- correlacion/controllers/line.js
+++ correlacion/controllers/line.js
@@ -45,11 +45,11 @@ const update = new RequestWrapper().hasId('projectId')
   .setHandler(async (req, resp) => {
     const { lineId, projectId, logId } = req.params
     const { notes } = req.body
-    const logOwner = await Log.findOne({ _id: req.params.logId, projectId: getIntValue(req.params.projectId) })
+    const logOwner = await Log.findOne({ _id: logId, projectId: getIntValue(req.params.projectId) })
     if (!logOwner) {
       throw { code: 404, msg: 'Log not found' }
     }
-    const lineUpdated = await Line.findOne({ _id: lineId, logId, log: logOwner._id, projectId: getIntValue(projectId) })
+    const lineUpdated = await Line.findOne({ _id: lineId, log: logOwner._id, projectId: getIntValue(projectId) })
     if (!lineUpdated) {
       throw {code: 404, msg: 'Line not found'}
     }
