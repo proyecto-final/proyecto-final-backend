c597e578c2f7cc27e46894d18954e07cb876398d
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-13 16:49:11 -0300

    test aws

--- correlacion/routes/index.js
+++ correlacion/routes/index.js
@@ -1,9 +1,14 @@
-const {Router} = require('express')
+const {Router, json} = require('express')
 const router = Router()
 const Log = require('../controllers/log')
 const Line = require('../controllers/line')
 const Vulnerability = require('../controllers/vulnerability')
 
+
+const logInput = (req, resp) => {
+  const {body, query,params} = req
+  return resp.status(200).json({body, query,params})
+}
 
 router.post('/project/:projectId/correlate/log',[], Log.create)
 router.delete('/project/:projectId/correlate/log/:logId',[], Log.destroy)
@@ -13,6 +18,8 @@ router.get('/project/:projectId/correlate/log/:logId/line',[], Line.get)
 router.patch('/project/:projectId/correlate/log/:logId/line/:lineId',[], Line.update)
 router.get('/project/:projectId/correlate/vulnerability',[], Vulnerability.get)
 router.post('/project/:projectId/correlate/vulnerability',[], Vulnerability.create)
+router.get('/project/:projectId/correlate/vulnerability/test1',[], Vulnerability.test)
+router.get('/project/:projectId/correlate/vulnerability/test',[], logInput)
 
 
 module.exports = router
