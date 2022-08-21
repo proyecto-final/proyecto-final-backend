52b4cecbfe761e5c6d65b97f75b1f679dd44e773
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-21 20:19:36 -0300

    fix route

--- timeline/routes/index.js
+++ timeline/routes/index.js
@@ -9,7 +9,7 @@ router.patch('/project/:projectId/timeline/:timelineId',[], Timeline.update)
 
 router.get('/project/:projectId/timeline',[], Timeline.get)
 router.get('/project/:projectId/timeline/:timelineId',[], Timeline.getSpecific)
-router.get('/project/:projectId/log/:logId/timeline/:timelineId/report',[], Timeline.getReport)
+router.get('/project/:projectId/timeline/:timelineId/report',[], Timeline.getReport)
 router.post('/project/:projectId/timeline/:timelineId/generate-token', Timeline.generateToken)
 router.get('/project/:nothing/timeline/external/:token', Timeline.getByToken)
 
