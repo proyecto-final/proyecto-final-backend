55ccf4436f1cc2b18e224001b1a90a48898fe63d
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-21 22:52:32 -0300

    Add models

---
+++ shared/models/ip.js
@@ -0,0 +1,51 @@
+
+module.exports = mongoose => {
+  const { Schema, model } = mongoose
+  const Ip = Schema({
+  raw: {
+    type: String,
+    required: true
+  },
+  reputation: {
+    type: Double,
+    required: true
+  },
+  isTor:{
+    type: Boolean,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  ASN: {
+    type: String,
+    required: true
+  },
+  ISP: {
+    type: String,
+    required: true
+  },
+  VPN: {
+    type: Boolean,
+    default: false
+  },
+  createdAt:{
+    type: Date,
+    default: Date.now
+    },
+   updatedAt:{
+    type: Date,
+    default: Date.now
+    },
+  })
+  Ip.methods.toJSON = function () {
+    const {__v, ...others} = this.toObject()
+    return others
+  }
+  return mongoose.models.ip || model('Ip', Ip)
+}
