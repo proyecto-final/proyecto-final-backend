f0e59dac4d1897df0b522ad39ad1a9f745c7d74b
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-22 23:39:38 -0300

    Update scheduler.js

--- ips/controllers/scheduler.js
+++ ips/controllers/scheduler.js
@@ -2,9 +2,8 @@ const cron = require('node-cron')
 const torList = require('./torList')
 
 cron.schedule(process.env.RUN_INTERVAL || '0 */15 * * * *', async () => {
-    // Read files
     try{
-        console.log('Refreshing Tor List')
+        console.log('Refreshing Tor List') //log to check if the cron is working
         await torList.refreshTorInfo()
     } catch(err) {
         console.log(err)
