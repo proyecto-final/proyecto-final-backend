4f4559b3a99a50ff0ce3b5e50ec6aedfc4503a36
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-22 21:02:46 -0300

    add shodan integration

---
+++ ips/controllers/ips.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+const RequestWrapper = require('../../shared/utils/requestWrapper')
+const { check } = require('express-validator')
+
+const SHODAN_API_KEY = process.env.SHODAN_API_KEY
+
+const getLocationInfo = new RequestWrapper(
+    check('ip', 'ip is required').isIP()
+).setHandler(async (req, res) => {
+    const {ip} = req.body
+    const {data} = await axios.get(`https://api.shodan.io/shodan/host/${ip}?key=${SHODAN_API_KEY}`)
+    res.status(200).json(data)
+}).wrap()
+
+
+
+module.exports = {
+    getLocationInfo
+}
