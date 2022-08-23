1ced744de946e62ea9f10c574e69154df742f783
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-22 21:06:24 -0300

    add swagger & fix get

--- ips/controllers/ips.js
+++ ips/controllers/ips.js
@@ -7,7 +7,7 @@ const SHODAN_API_KEY = process.env.SHODAN_API_KEY
 const getLocationInfo = new RequestWrapper(
     check('ip', 'ip is required').isIP()
 ).setHandler(async (req, res) => {
-    const {ip} = req.body
+    const {ip} = req.query
     const {data} = await axios.get(`https://api.shodan.io/shodan/host/${ip}?key=${SHODAN_API_KEY}`)
     res.status(200).json(data)
 }).wrap()
