b405488fa373aa34e8857a51ae9a73edf21d619e
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-08-24 19:12:50 -0300

    Update timeline.js

--- timeline/controllers/timeline.js
+++ timeline/controllers/timeline.js
@@ -17,7 +17,7 @@ const getReport = new RequestWrapper()
   .setHandler(async (req, res) => {
     const { timelineId, projectId } = req.params
     const timeline = await Timeline.findOne({_id: timelineId, projectId: getIntValue(projectId)}).populate('lines.vulnerabilites')
-    const logs = await Log.find({_id: {$in: timeline.logs}})
+    const logs = await Log.find({_id: {$in: timeline?.logs}})
     const logLines = await Line.find({log: {$in: logs}})
     if(!logs || logs.length === 0){
       throw {code: 404, msg: 'Log not found'}
