1f3e4ae335657bdb00441d2466e0edd30170dee1
rdiazutn <rodrigo.diaz@frba.utn.edu.ar>
2022-08-23 21:20:32 -0300

    fix

--- ips/controllers/scheduler.js
+++ ips/controllers/scheduler.js
@@ -6,7 +6,7 @@ const axios = require('axios')
 cron.schedule(process.env.RUN_INTERVAL || '0 */15 * * * *', async () => {
   try{
     console.log('Refreshing Tor List') //log to check if the cron is working 
-    const data = await axios.$get('https://check.torproject.org/torbulkexitlist')
+    const {data} = await axios.get('https://check.torproject.org/torbulkexitlist')
     //clean collection
     await TorList.deleteMany({})
     const torList = data.split('\n').map(ip => ip.trim()).filter(ip => !!ip).map(ip => (new TorList({ip})))
