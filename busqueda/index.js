efe1af0375c031a8270557cd08f6f6d8d2331735
BugMaker <105680112+bugmaker13@users.noreply.github.com>
2022-09-24 12:30:30 -0300

    Chore/sc 491/automatizar pipelines (#99)
    * Remove unused folders and add steps in pipeline
    
    * add ecs cli dependency to pipeline
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * fix health services
    
    * Update prod-back-public.yml
    
    * add apigateway integration for evtx converter
    
    * edit local files
    
    * add retry logic on mongo connection
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * may god help us all
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * fix lambda and split request to mongodb
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml
    
    * Update prod-back-public.yml

--- busqueda/index.js
+++
@@ -1,2 +0,0 @@
-const Server = require('./server/app')
-const app = new Server()
--- correlacion/index.js
+++ correlacion/index.js
@@ -29,18 +29,22 @@ app.listen(process.env.PORT, () => {
 })
 
 
+const connect2Mongo = () => {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedtopology: true
+  }).then(() => {
+    console.log('Connected to MongoDB')
+  }).catch(err => {
+    console.log(err)
+    setTimeout(() => {
+      console.log('Database not ready yet. Trying to re-connect')
+      connect2Mongo()
+    }, 3000) //wait 3 seconds before reconnect
+  })
+}
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedtopology: true
-}).then(() => {
-  console.log('Connected to MongoDB')
-}).catch(err => {
-  console.log(err)
-})
-
-
-
+connect2Mongo()
 const swaggerDocument = YAML.load('./swagger.yaml')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
